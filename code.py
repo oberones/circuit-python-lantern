973eb71505402fb1c476609fce2c4daebaf51d30
Enoch Root <oberones@gmail.com>
2023-10-10 16:30:59 -0400

    feat: add garbage collection to clean up memory use
    Adding garbage collection to try to combat frequent memory issues

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import board
-import math
 import time
 import random
+import gc
 from adafruit_debouncer import Debouncer
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_circuitplayground import cp
@@ -16,7 +16,6 @@ button_b_pin = DigitalInOut(board.BUTTON_B)
 button_b_pin.direction = Direction.INPUT
 button_b_pin.pull = Pull.DOWN
 button_b_debounced = Debouncer(button_b_pin)
-
 
 def config_fire():
     while True:
@@ -51,6 +50,9 @@ def interpolate_color(color1, color2, steps):
         g = int((1.0 - step_size * i) * color1[1] + step_size * i * color2[1])
         b = int((1.0 - step_size * i) * color1[2] + step_size * i * color2[2])
         interp_colors.append((r, g, b))
+    print("interpolate color gc")
+    gc.collect()
+    print(gc.mem_free())
     return interp_colors
 
 # Function to cycle through the color spectrum
@@ -61,6 +63,9 @@ def config_spectrum():
         next_color = colors[(i + 1) % len(colors)]
         transition_steps = 50
         i_colors = interpolate_color(current_color, next_color, transition_steps)
+        print("config spectrum gc")
+        gc.collect()
+        print(gc.mem_free())
         for step_color in i_colors:
             cp.pixels.fill(step_color)
             cp.pixels.show()
@@ -72,7 +77,6 @@ def config_spectrum():
         time.sleep(0.5)
         i = (i + 1) % len(colors)
 
-
 def complementary_color(color):
     return (255 - color[0], 255 - color[1], 255 - color[2])
 
@@ -80,33 +84,29 @@ def blend_colors(color1, color2, weight=0.5):
     r = int(color1[0] * weight + color2[0] * (1 - weight))
     g = int(color1[1] * weight + color2[1] * (1 - weight))
     b = int(color1[2] * weight + color2[2] * (1 - weight))
+    print("blend colors gc")
+    gc.collect()
+    print(gc.mem_free())
     return (r, g, b)
 
 def config_psychedelic():
     while True:
         for i in range(len(colors)):  # Base layer: spectrum animation
             base_color = colors[i]
-            comp_color = complementary_color(base_color)  # Get the complementary color
-
-            # Interpolate colors for smooth transition
             transition_steps = 30
             next_color = colors[(i + 1) % len(colors)]
             interp_colors = interpolate_color(base_color, next_color, transition_steps)
-
+            print("config psychedelic gc")
+            gc.collect()
+            print(gc.mem_free())
             for step_color in interp_colors:
                 cp.pixels.fill(step_color)  # Fill all LEDs with the base color
-
-                # Top layer: two dynamic LEDs with complementary color
                 for j in range(10):  # Move the dynamic LEDs around the circle
                     blend_color = blend_colors(
                         step_color, complementary_color(step_color)
                     )
-
-                    # Set main dynamic LEDs
                     cp.pixels[j] = complementary_color(step_color)
                     cp.pixels[(j + 5) % 10] = complementary_color(step_color)
-
-                    # Set blending effect for neighboring LEDs
                     cp.pixels[(j + 1) % 10] = blend_color
                     cp.pixels[(j - 1) % 10] = blend_color
                     cp.pixels[(j + 6) % 10] = blend_color
@@ -118,11 +118,8 @@ def config_psychedelic():
                     if button_a_debounced.fell or button_b_debounced.fell:
                         return
                     time.sleep(0.05)
-
-                    # Reset LEDs for next iteration
                     for k in range(10):
                         cp.pixels[k] = step_color
-
 
 current_config = 0
 while True:
@@ -134,4 +131,7 @@ while True:
         current_config = (current_config + 1) % (max_config + 1)
     button_a_debounced.update()
     button_b_debounced.update()
+    print("main gc")
+    gc.collect()
+    print(gc.mem_free())
     config_map[current_config]()
